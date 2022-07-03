60a03fb007d2428217a044725b435f5f0b729690
polyachenkov_aleksey <devel.js@yandex.ru>
2022-07-03 16:52:55 +0300

    complete task 3.6

---
+++ 02-components/06-MeetupView/components/MeetupAgendaItem.js
@@ -0,0 +1,40 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from '../api/meetupService';
+
+export default defineComponent({
+  name: 'MeetupAgendaItem',
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      agendaItemIcons,
+      agendaItemDefaultTitles,
+    };
+  },
+  computed: {
+    title() {
+      return this.agendaItem.title ? this.agendaItem.title : this.agendaItemDefaultTitles[this.agendaItem.type];
+    },
+  },
+
+  template: `
+    <div class="agenda-item">
+      <div class="agenda-item__col">
+        <img :src="\`/assets/icons/icon-\${agendaItemIcons[agendaItem.type]}.svg\`" class="icon" alt="key" />
+      </div>
+      <div class="agenda-item__col">{{ agendaItem.startsAt }} - {{ agendaItem.endsAt }}</div>
+      <div class="agenda-item__col">
+        <h3 class="agenda-item__title">{{ title }}</h3>
+        <p class="agenda-item__talk" v-if="agendaItem.type === 'talk'">
+          <span>{{ agendaItem.speaker }}</span>
+          <span class="agenda-item__dot"></span>
+          <span class="agenda-item__lang">{{ agendaItem.language }}</span>
+        </p>
+        <p v-show="agendaItem.description">{{ agendaItem.description }}</p>
+      </div>
+    </div>`,
+});
