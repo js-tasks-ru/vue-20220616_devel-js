dfad8a26511777381b85972d2973cab6ee45912e
polyachenkov_aleksey <devel.js@yandex.ru>
2022-07-03 15:21:55 +0300

    fix task 2.2

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -3,28 +3,18 @@ import { createApp } from './vendor/vue.esm-browser.js';
 createApp({
   data() {
     return {
-      total: 0,
       firstNumber: null,
       secondNumber: null,
       operation: null,
     };
   },
-  watch: {
-    operation() {
-      switch (this.operation) {
-        case 'sum':
-          this.total = this.firstNumber + this.secondNumber;
-          break;
-        case 'subtract':
-          this.total = this.firstNumber - this.secondNumber;
-          break;
-        case 'multiply':
-          this.total = this.firstNumber * this.secondNumber;
-          break;
-        case 'divide':
-          this.total = this.firstNumber / this.secondNumber;
-          break;
-      }
+  computed: {
+    total() {
+      if (this.operation === 'sum') return this.firstNumber + this.secondNumber;
+      else if (this.operation === 'subtract') return this.firstNumber - this.secondNumber;
+      else if (this.operation === 'multiply') return this.firstNumber * this.secondNumber;
+      else if (this.operation === 'divide') return this.firstNumber / this.secondNumber;
+      else return null;
     },
   },
 }).mount('#app');
