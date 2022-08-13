42a48d241bb560a1c9342c1fc4b9b350128241b0
polyachenkov_aleksey <devel.js@yandex.ru>
2022-08-13 19:14:57 +0300

    complete task 7.1

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,32 @@
 <template>
-  <button></button>
+  <button @click="increaseCount">{{ countValue }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+  emits: ['update:count'],
+  data() {
+    return {
+      countValue: this.count,
+    };
+  },
+  watch: {
+    count() {
+      this.countValue = this.count;
+    },
+  },
+  methods: {
+    increaseCount() {
+      this.$emit('update:count', this.countValue + 1);
+      this.countValue++;
+    },
+  },
 };
 </script>
